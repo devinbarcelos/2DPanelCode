975ea9b50aa3d04091552297fe70bc0c182cb0b0
devinbarcelos <devinbarcelos@gmail.com>
2017-02-24 01:00:52 -0500

    Updates to begin influence matrix calculation

--- infcoeff.m
+++ infcoeff.m
@@ -1,19 +1,26 @@
-function [ ] = infcoeff(valN, vecEPS, matCP)
+function [indvel] = infcoeff(vecEPS, matCP)
 % This function calculated the influence coefficients
 %
 % Uses the zero velocity normal to airfoil as a boundary condition:
 % (q+Q_inf) dot norm = 0
 
-% Global to local
-tcos = cos(vecEPS);
-tsin = sin(vecEPS);
-
+len = length(vecEPS);
 % Calculate distance between control points of induced againts inducers
-vecXINFPTS = reshape(repmat(matCP(:,1)',[valN,1]),[valN^2,1]) - repmat(matCP(:,1),[valN,1]);
-vecYINFPTS = reshape(repmat(matCP(:,2)',[valN,1]),[valN^2,1]) - repmat(matCP(:,2),[valN,1]);
+vecINDUCEDX = reshape(repmat(matCP(:,1)',[len,1]),[len^2,1]);
+vecINDUCERX = repmat(matCP(:,1),[len,1]);
+
+vecINDUCEDY = reshape(repmat(matCP(:,2)',[len,1]),[len^2,1]);
+vecINDUCERY = repmat(matCP(:,2),[len,1]);
+
+% Use a circulation of one
+vecGAMMA = ones(len^2,1);
 
 % Calculate induced velocities
-vecU = [tcos tsin; -tsin cos];
+[ indvel ] = ind_vel(vecEPS, vecGAMMA, vecINDUCEDX, vecINDUCEDY, vecINDUCERX, vecINDUCERY);
+
+% Calculate influence matrix by doting the induced velocities by the
+% normals
+
 
 end
 
--- main.m
+++ main.m
@@ -36,36 +36,36 @@ disp(s)
 
 valALPHA = 5; % Angle of attack (deg)
 
-%% ===================== Intialize Cylinder Geometry =================== %%
-% Comment out this block if the cylinder case is not being used
+% %% ===================== Intialize Cylinder Geometry =================== %%
+% % Comment out this block if the cylinder case is not being used
+% 
+% valR = 1; % Cylinder radius
+% valN = 100; % Number of panels
+% 
+% % Pass cylinder information to cylinder generation function
+% % Returns coordinates of each node
+% [matNODES] = cyn_panel(valR, valN); 
 
-valR = 1; % Cylinder radius
-valN = 100; % Number of panels
-
-% Pass cylinder information to cylinder generation function
-% Returns coordinates of each node
-[matNODES] = cyn_panel(valR, valN); 
-
-%% ========================= Airfoil Case ============================== %%
-% Comment out this block if the airfoil case is not being used
-
-valNACA = 2412; % 4-Digit airfoil
-valC = 1; % Approximate Chord length
-valN = 50; % Approximate number of panels
-
-% Pass airfoil information to airfoil generation function
-
-% The airfoil generation function uses a the equaton of the NACA 4-digit
-% airfoil (https://en.wikipedia.org/wiki/NACA_airfoil).
-
-% This equation generates an airfoil with a blunt trailing edge, which will
-% complicate the source code. Therefore, a sharp trailing edge is added by 
-% the function. Because of this, the chord length and the number of panels 
-% will not be exactly the same as entered into the function. The function
-% returns the true number of panels, and the true chord length
-%
-% Returns coordinates of each node and stores them in node stucture
-[matNODES, valC, valN] = airfoil_panel(valNACA, valC, valN);
+% %% ========================= Airfoil Case ============================== %%
+% % Comment out this block if the airfoil case is not being used
+% 
+% valNACA = 2412; % 4-Digit airfoil
+% valC = 1; % Approximate Chord length
+% valN = 50; % Approximate number of panels
+% 
+% % Pass airfoil information to airfoil generation function
+% 
+% % The airfoil generation function uses a the equaton of the NACA 4-digit
+% % airfoil (https://en.wikipedia.org/wiki/NACA_airfoil).
+% 
+% % This equation generates an airfoil with a blunt trailing edge, which will
+% % complicate the source code. Therefore, a sharp trailing edge is added by 
+% % the function. Because of this, the chord length and the number of panels 
+% % will not be exactly the same as entered into the function. The function
+% % returns the true number of panels, and the true chord length
+% %
+% % Returns coordinates of each node and stores them in node stucture
+% [matNODES, valC, valN] = airfoil_panel(valNACA, valC, valN);
 
 %% ========================= Flat Plate Case =========================== %%
 % Comment out this block if flat plate is not being used
@@ -81,29 +81,30 @@ matNODES = flat_plate(valC, valN);
 % (nx, ny)s
 [matCP, vecS, matTANG, matNORM, vecEPS] = control_point(matNODES);
 
-
-%% ============================ Plot Geometry ========================== %%
-
-% Plots the nodes, panel control point, tangent vectors, and normal vectors
-
-valSCALE = 0.05; % Scales size of vectors for ease of viewing in plot
-
-close all
-figure
-hold on
-plot(matNODES(:, 1), matNODES(:, 2), '--o')
-plot(matCP(:, 1), matCP(:, 2), 'rx')
-axis equal 
-grid on
-for j = 1:1:size(matCP, 1)
-    % plot tangents
-    quiver(matCP(j, 1),matCP(j, 2), valSCALE.*matTANG(j, 1), ...
-        valSCALE.*matTANG(j, 2), 'r')
-    
-    % plot normals
-    quiver(matCP(j, 1),matCP(j, 2), valSCALE.*matNORM(j, 1),...
-        valSCALE.*matNORM(j, 2), 'm')
-end
-hold off
+%% ================== Calculate Influence Matrix ======================= %%
+[indvel] = infcoeff(vecEPS, matCP);
+% %% ============================ Plot Geometry ========================== %%
+% 
+% % Plots the nodes, panel control point, tangent vectors, and normal vectors
+% 
+% valSCALE = 0.05; % Scales size of vectors for ease of viewing in plot
+% 
+% close all
+% figure
+% hold on
+% plot(matNODES(:, 1), matNODES(:, 2), '--o')
+% plot(matCP(:, 1), matCP(:, 2), 'rx')
+% axis equal 
+% grid on
+% for j = 1:1:size(matCP, 1)
+%     % plot tangents
+%     quiver(matCP(j, 1),matCP(j, 2), valSCALE.*matTANG(j, 1), ...
+%         valSCALE.*matTANG(j, 2), 'r')
+%     
+%     % plot normals
+%     quiver(matCP(j, 1),matCP(j, 2), valSCALE.*matNORM(j, 1),...
+%         valSCALE.*matNORM(j, 2), 'm')
+% end
+% hold off
 
 
