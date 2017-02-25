2dc907ccda4f519d7ba3f6c55a5b8113fec11054
devinbarcelos <devinbarcelos@gmail.com>
2017-02-24 19:00:19 -0500

    Calculates induced velocities

--- ind_vel.m
+++ ind_vel.m
@@ -1,21 +1,22 @@
-function [ indvel ] = ind_vel(vecEPS, vecGAMMA, vecINDUCEDX, vecINDUCEDY, vecINDUCERX, vecINDUCERY)
+function [ matINDVEL ] = ind_vel(vecQ, vecEPS, vecS, vecINDUCEDX, vecINDUCEDY, vecINDUCERX, vecINDUCERY)
 % Calculate induced velocities
 %
 %   INPUTS
-%   vecGAMMA - vector of circulation
+%   vecQ - Source strength
+%   vecEPS - Panel epsilon
+%   vecS - Panel length
 %   vecINDUCEDX - induced x points
 %   vecINDUCEDY - induced y points
 %   vecINDUCERX - inducer x points
 %   vecINDUCERY - indecer y points
 %
 %   OUTPUTS
-%   indvel - induced velocities (x,y)
+%   matINDVEL - induced velocities (x,y)
 
 % Difference between induced and inducers
 delx = vecINDUCEDX - vecINDUCERX;
 dely = vecINDUCEDY - vecINDUCERY;
 del = [delx,dely];
-r = delx.^2+dely.^2;
 
 len = size(vecEPS);
 len = len(1);
@@ -29,13 +30,23 @@ matTRANS(1,2,:) = reshape(repmat(tsin,[len,1]),[1,1,len^2]);
 matTRANS(2,1,:) = reshape(repmat(-tsin,[len,1]),[1,1,len^2]);
 matTRANS(2,2,:) = reshape(repmat(tcos,[len,1]),[1,1,len^2]);
 
-% Calculate induced velocity 
+% Calculate X_q and Y_q using matTRANS*(x_i-x_j)
+% Note i is induced and j is unducer
 for i = 1:len^2
-    indvel(i,:) = matTRANS(:,:,i)*(del(i,:)');
+    xy_iq(i,:) = matTRANS(:,:,i)*(del(i,:)');
 end
 
-indvel = (vecGAMMA./(2*pi*r.^2)).*indvel;
+% Repmat the S vector by the number of panels
+vecS = repmat(vecS,[len,1]);
 
+% Repmat the source strength vector by the number of panels
+vecQ = repmat(vecQ,[len,1]);
+
+% Calculate the induced velocities
+vecVxq = -0.5*log(((xy_iq(:,1)+vecS/2).^2+xy_iq(:,2).^2)./((xy_iq(:,1)-vecS/2).^2+xy_iq(:,2).^2));
+vecVyq = -1*(atan((xy_iq(:,1)+vecS/2)./xy_iq(:,2))-atan((xy_iq(:,1)-vecS/2)./xy_iq(:,2)));
+
+matINDVEL = [vecQ.*vecVxq,vecQ.*vecVyq];
 
 end
 
