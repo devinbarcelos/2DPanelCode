68017a05f141017b8c455b7e251bf43a84034323
devinbarcelos <devinbarcelos@gmail.com>
2017-02-25 01:14:57 -0500

    Updated main to calculate upto and including pressure distributions

--- main.m
+++ main.m
@@ -34,7 +34,7 @@ disp(s)
 %% =============================== Input =============================== %%
 % These are the required inputs
 
-valALPHA = 5; % Angle of attack (deg)
+valALPHA = 10; % Angle of attack (deg)
 
 % %% ===================== Intialize Cylinder Geometry =================== %%
 % % Comment out this block if the cylinder case is not being used
@@ -45,27 +45,27 @@ valALPHA = 5; % Angle of attack (deg)
 % % Pass cylinder information to cylinder generation function
 % % Returns coordinates of each node
 % [matNODES] = cyn_panel(valR, valN); 
-
-%% ========================= Airfoil Case ============================== %%
-% Comment out this block if the airfoil case is not being used
-
-valNACA = 2412; % 4-Digit airfoil
-valC = 1; % Approximate Chord length
-valN = 101; % Approximate number of panels
-
-% Pass airfoil information to airfoil generation function
-
-% The airfoil generation function uses a the equaton of the NACA 4-digit
-% airfoil (https://en.wikipedia.org/wiki/NACA_airfoil).
-
-% This equation generates an airfoil with a blunt trailing edge, which will
-% complicate the source code. Therefore, a sharp trailing edge is added by 
-% the function. Because of this, the chord length and the number of panels 
-% will not be exactly the same as entered into the function. The function
-% returns the true number of panels, and the true chord length
-%
-% Returns coordinates of each node and stores them in node stucture
-[matNODES, valC, valN] = airfoil_panel(valNACA, valC, valN);
+% 
+% %% ========================= Airfoil Case ============================== %%
+% % Comment out this block if the airfoil case is not being used
+% 
+% valNACA = 2412; % 4-Digit airfoil
+% valC = 1; % Approximate Chord length
+% valN = 101; % Approximate number of panels
+% 
+% % Pass airfoil information to airfoil generation function
+% 
+% % The airfoil generation function uses a the equaton of the NACA 4-digit
+% % airfoil (https://en.wikipedia.org/wiki/NACA_airfoil).
+% 
+% % This equation generates an airfoil with a blunt trailing edge, which will
+% % complicate the source code. Therefore, a sharp trailing edge is added by 
+% % the function. Because of this, the chord length and the number of panels 
+% % will not be exactly the same as entered into the function. The function
+% % returns the true number of panels, and the true chord length
+% %
+% % Returns coordinates of each node and stores them in node stucture
+% [matNODES, valC, valN] = airfoil_panel(valNACA, valC, valN);
 
 % %% ========================= Flat Plate Case =========================== %%
 % % Comment out this block if flat plate is not being used
@@ -73,11 +73,11 @@ valN = 101; % Approximate number of panels
 % valN = 5;
 % matNODES = flat_plate(valC, valN);
 
-% %% ========================= Input File Case =========================== %%
-% % Comment out this block if inout file is not being used to generate
-% % geometry
-% strFILE =  'test.txt';
-% matNODES = input_func(strFILE);
+%% ========================= Input File Case =========================== %%
+% Comment out this block if inout file is not being used to generate
+% geometry
+strFILE =  'naca.txt';
+matNODES = input_func(strFILE);
 
 %% ====================== Control Points =============================== %%
 
@@ -88,7 +88,7 @@ valN = 101; % Approximate number of panels
 [matCP, vecS, matTANG, matNORM, vecEPS] = control_point(matNODES);
 
 %% ==================== Calculate Influence Matrix ===================== %%
-[matINFCOEFF] = infcoeff(vecS, vecEPS, matCP, matNORM);
+[matINFCOEFFT, matINFCOEFF, vecINDUCEDX, vecINDUCERX, vecINDUCEDY, vecINDUCERY] = infcoeff(vecS, vecEPS, matCP, matNORM, matTANG);
 
 %% ================== Calculate Freestream Velocity ==================== %%
 [vecUINF] = uinf(valALPHA);
@@ -99,19 +99,23 @@ valN = 101; % Approximate number of panels
 %% ==================== Solve for Source Strength ====================== %%
 [vecQ] = source_strength(vecR, matINFCOEFF);
 
-% %% ============================ Plot Geometry ========================== %%
-% 
-% % Plots the nodes, panel control point, tangent vectors, and normal vectors
-% 
-% valSCALE = 0.05; % Scales size of vectors for ease of viewing in plot
-% 
-% close all
-% figure
-% hold on
-% plot(matNODES(:, 1), matNODES(:, 2), '--o')
-% plot(matCP(:, 1), matCP(:, 2), 'rx')
-% axis equal 
-% grid on
+%% ================== Calculate Pressure Distribution ================== %%
+% Calcualte pressure
+[vecPRESSURE] = pressure(vecQ, matINFCOEFFT, matTANG);
+
+%% ============================ Plot Geometry ========================== %%
+
+% Plots the nodes, panel control point, tangent vectors, and normal vectors
+
+valSCALE = 1; % Scales size of vectors for ease of viewing in plot
+
+close all
+figure
+hold on
+plot(matNODES(:, 1), matNODES(:, 2), '-')
+%plot(matCP(:, 1), matCP(:, 2), '-*')
+axis equal 
+grid on
 % for j = 1:1:size(matCP, 1)
 %     % plot tangents
 %     quiver(matCP(j, 1),matCP(j, 2), valSCALE.*matTANG(j, 1), ...
@@ -121,6 +125,8 @@ valN = 101; % Approximate number of panels
 %     quiver(matCP(j, 1),matCP(j, 2), valSCALE.*matNORM(j, 1),...
 %         valSCALE.*matNORM(j, 2), 'm')
 % end
-% hold off
+
+plot(matCP(:,1),vecPRESSURE)
+hold off
 
 
