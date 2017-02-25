9ad27a352158435c37ab0f74b2ae86429647deaa
devinbarcelos <devinbarcelos@gmail.com>
2017-02-25 01:10:50 -0500

    Corrected influence coeff calculation

--- infcoeff.m
+++ infcoeff.m
@@ -1,4 +1,4 @@
-function [matINFCOEFF] = infcoeff(vecS, vecEPS, matCP, matNORM)
+function [matINFCOEFFT, matINFCOEFF, vecINDUCEDX, vecINDUCERX, vecINDUCEDY, vecINDUCERY] = infcoeff(vecS, vecEPS, matCP, matNORM, matTANG)
 % This function calculates the influence coefficients
 % Uses the zero velocity normal to airfoil as the kinimatic boundary
 % condition
@@ -38,6 +38,18 @@ vecQ = ones(len,1);
 % Calculate influence matrix by doting the induced velocities by the
 % normals (vectorized)
 matNORM = reshape(repmat(reshape(matNORM,[1,len*2]),[len,1]),[len^2,2]);
+
 matINFCOEFF = dot(matINDVEL,matNORM,2);
 matINFCOEFF = reshape(matINFCOEFF,[len,len])';
+
+matTANG = reshape(repmat(reshape(matTANG,[1,len*2]),[len,1]),[len^2,2]);
+matINFCOEFFT = dot(matINDVEL,matTANG,2);
+matINFCOEFFT = -1*reshape(matINFCOEFFT,[len,len])';
+
+
+for i =1:len
+    matINFCOEFF(i,i) = pi;
+end
+
+
 end
